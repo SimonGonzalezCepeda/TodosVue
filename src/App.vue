0462c6d685e1a06c6d0c74ea811d09d16318c4fe
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2017-01-13 15:55:57 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    Now we start from 0
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+
+</style>
