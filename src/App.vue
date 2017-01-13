0477437566d3b40c574f4457e948363aa3597e30
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2017-01-13 16:41:26 +0100

    Solver issue with VueMaterial. Need custom CSS

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,6 @@
       <h2 class="md-title">Todos</h2>
     </md-toolbar>
 
-    <div>
-      <md-button class="md-raised md-accent" @click="toggleRightSidenav">Toggle right</md-button>
-      <p>Open console to see the events</p>
-    </div>
-
     <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
       <md-toolbar class="md-large">
         <div class="md-toolbar-container">
@@ -20,18 +15,67 @@
         </div>
       </md-toolbar>
 
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi cupiditate esse necessitatibus beatae nobis, deserunt ut est fugit, tempora deleniti, eligendi commodi doloribus. Nemo, assumenda possimus, impedit inventore perferendis iusto!</p>
+      <md-list>
+        <md-subheader>Navigation</md-subheader>
+
+        <md-list-item>
+          <md-icon>move_to_inbox</md-icon> <span>Inbox</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>send</md-icon> <span>Outbox</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>delete</md-icon> <span>Trash</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>error</md-icon> <span>Spam</span>
+
+          <md-divider class="md-inset"></md-divider>
+        </md-list-item>
+
+        <md-subheader>Contacts</md-subheader>
+
+        <md-list-item>
+          <md-avatar>
+            <img src="assets/avatar-2.jpg" alt="People">
+          </md-avatar>
+
+          <span>Abbey Christansen</span>
+
+          <md-button class="md-icon-button md-list-action">
+            <md-icon class="md-primary">chat_bubble</md-icon>
+          </md-button>
+        </md-list-item>
+
+        <md-list-item>
+          <md-avatar>
+            <img src="assets/avatar-2.jpg" alt="People">
+          </md-avatar>
+
+          <span>Alex Nelson</span>
+
+          <md-button class="md-icon-button md-list-action">
+            <md-icon class="md-primary">chat_bubble</md-icon>
+          </md-button>
+        </md-list-item>
+
+        <md-list-item>
+          <md-avatar>
+            <img src="assets/avatar-2.jpg" alt="People">
+          </md-avatar>
+
+          <span>Mary Johnson</span>
+
+          <md-button class="md-icon-button md-list-action">
+            <md-icon>chat_bubble</md-icon>
+          </md-button>
+        </md-list-item>
+      </md-list>
     </md-sidenav>
 
-    <md-sidenav class="md-right" ref="rightSidenav" @open="open('Right')" @close="close('Right')">
-      <md-toolbar>
-        <div class="md-toolbar-container">
-          <h3 class="md-title">Sidenav content</h3>
-        </div>
-      </md-toolbar>
-
-      <md-button class="md-raised md-accent" @click="closeRightSidenav">Close</md-button>
-    </md-sidenav>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
+import VueRouter from 'vue-router'
+
+Vue.use(VueMaterial)
+Vue.use(VueRouter)
 
 /* eslint-disable no-new */
 new Vue({
