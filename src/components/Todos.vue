f2edd663c93311506734c56d1449205a0063e518
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2017-01-17 21:56:35 +0100

    Added code to execute todosBackend XHR response

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -14,6 +14,23 @@
       return {
         msg: 'Todos aqui'
       }
+    },
+    created () {
+      console.log('Provant que va!')
+      this.fetchData()
+    },
+    methods: {
+      fetchData: function () {
+        return this.fetchPage(1)
+      },
+      fetchPage: function (page) {
+        this.$http.get('http://todos.dev:8080/api/v1/task?pages=' + page).then((response) => {
+          console.log(response.data)
+          this.todos = response.data.data
+        }, (response) => {
+          console.log(response.data)
+        })
+      }
     }
   }
 </script>
