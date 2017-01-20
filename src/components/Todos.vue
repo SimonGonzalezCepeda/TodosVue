9c976ff79b6612a0079b0b785a533b5b39516874
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2017-01-20 15:53:19 +0100

    Followed class. Late commit

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,23 +1,44 @@
 <template>
-  <div>
-    <div>Msg: {{msg}}</div>
-  </div>
+    <div>
+        <div v-show="!authorized">
+            <md-button class="md-raised md-warn">Connect</md-button>
+        </div>
+        <div v-show="!logut">
+            <md-button class="md-raised md-warn">Log out</md-button>
+        </div>
+        <ul v-show="!authorized">
+            <li vi-for></li>
+        </ul>
+    </div>
 </template>
 <style>
   body{
     background-color:#ff0000;
   }
+
 </style>
 <script>
+  var STORAGE_KEY = 'todosvue_token'
+  var AUTH_CLIENT_ID = 5
+  var AUTH_REDIRECT_URI = ''
+
   export default {
     data () {
       return {
-        msg: 'Todos aqui'
+        todos: [],
+        authorized: false
       }
     },
     created () {
-      console.log('Provant que va!')
-      this.fetchData()
+      var token = extractToken(document.location.hash)
+      console.log(token)
+      if (token) this.saveToken(token)
+      console.log(this.fetchToken)
+      if(this.fetchToken()){
+        this.authorized = true
+      } else {
+        this.authorized = false
+      }
     },
     methods: {
       fetchData: function () {
@@ -30,7 +51,29 @@
         }, (response) => {
           console.log(response.data)
         })
+      },
+      extractToken : function (hash) {
+        var match = has.match(/acces_token=(\w+)/)
+        return !!match && match[1]
+      },
+      connect : function() {
+        console.log('do conect here!')
+        query = [
+          client_id: AUTH_CLIENT_ID,
+          redirect_uri: AUTH_REDIRECT_URI,
+          response_type: 'token',
+          scope: ''
+        ]
+        var query = window.querystring.stringify(query)
+        window.location.replace('http://todos.dev:8080/oauth/authorize?' + query)
+      },
+      fetchToken : function () {
+        return window.localStorage.getItem(STORAGE_KEY)
+      },
+      saveToken : function () {
+        window.localStorage.setItem(STORAGE_KEY, token)
       }
     }
   }
+
 </script>
